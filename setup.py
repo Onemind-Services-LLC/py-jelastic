f9702ada220de8f8ce6ab5bfb3147093185847d7
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-15 18:07:56 +0530

    adds workflows

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-jelastic',
+    description='Jelastic API client library',
+    url='https://github.com/Onemind-Services-LLC/py-jelastic',
+    author='Abhimanyu Saharan',
+    author_email='asaharan@onemindservices.com',
+    license='Apache2',
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=[
+        'requests',
+        'httpx',
+    ],
+    zip_safe=False,
+    keywords=['jelastic', 'api', 'client'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
