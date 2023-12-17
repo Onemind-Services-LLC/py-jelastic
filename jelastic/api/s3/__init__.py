b6b79778d0c8395f05e97e7bd4c2fc263934d201
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-17 19:18:27 +0530

    added S3 Account class

--- jelastic/api/s3/__init__.py
+++ jelastic/api/s3/__init__.py
@@ -4,4 +4,134 @@ __all__ = ["S3"]
 
 
 class S3(ClientAbstract):
+    """
+    >>> from jelastic import Jelastic
+    >>> jelastic = Jelastic('https://jca.xapp.cloudmydc.com', token='d6f4e314a5b5fefd164995169f28ae32d987704f')
+    >>> jelastic.s3
+
+    Ref: https://docs.jelastic.com/api/private/#!/api/s3
+    """
     _endpoint1 = "s3"
+
+    @property
+    def Account(self) -> "_Account":
+        """
+        Service provides an interface for managing S3 stuff
+
+        >>> from jelastic import Jelastic
+        >>> jelastic = Jelastic('https://jca.xapp.cloudmydc.com', token='d6f4e314a5b5fefd164995169f28ae32d987704f')
+        >>> jelastic.s3.Account
+
+        Ref: https://docs.jelastic.com/api/private/#!/api/s3.Account
+        """
+        return _Account(
+            session=self._session,
+            token=self._token,
+            debug=self._debug
+        )
+
+
+class _Account(S3):
+    """
+    Service provides an interface for managing S3 stuff
+
+    Ref: https://docs.jelastic.com/api/private/#!/api/s3.Account
+    """
+    _endpoint2 = "account"
+
+    def Create(self, host_group: str, name: str, owner_uid: int = None):
+        """
+        Creates a new S3 account.
+
+        :param host_group: unique name of the target host group.
+        :param name: a name of the account to be created.
+        :param owner_uid: unique identifier of the S3 account owner.
+        """
+        return self._get(
+            "Create",
+            params={
+                "hostGroup": host_group,
+                "name": name,
+                "ownerUid": owner_uid
+            }
+        )
+
+    def Delete(self, host_group: str, name: str, owner_uid: int = None):
+        """
+        Deletes an S3 account.
+
+        :param host_group: unique name of the target host group.
+        :param name: a name of the account to be deleted.
+        :param owner_uid: unique identifier of the S3 account owner.
+        """
+        return self._get(
+            "Delete",
+            params={
+                "hostGroup": host_group,
+                "name": name,
+                "ownerUid": owner_uid
+            }
+        )
+
+    def GenerateKey(self, host_group: str, name: str, owner_uid: int = None):
+        """
+        Generates a new key for an S3 account.
+
+        :param host_group: unique name of the target host group.
+        :param name: a name of the account to be updated.
+        :param owner_uid: unique identifier of the S3 account owner.
+        """
+        return self._get(
+            "GenerateKey",
+            params={
+                "hostGroup": host_group,
+                "name": name,
+                "ownerUid": owner_uid
+            }
+        )
+
+    def GetKeys(self, owner_uid: int = None):
+        """
+        Returns the list of accounts together with keys for each available S3 cluster.
+
+        :param owner_uid: unique identifier of the S3 account owner.
+        """
+        return self._get(
+            "GetKeys",
+            params={
+                "ownerUid": owner_uid
+            }
+        )
+
+    def RegenerateKeys(self, host_group: str, acc_key: str, owner_uid: int = None):
+        """
+        Regenerates the target key pair by access key.
+
+        :param host_group: unique name of the target host group.
+        :param acc_key: access key that should be regenerated.
+        :param owner_uid: unique identifier of the S3 account owner.
+        """
+        return self._get(
+            "RegenerateKeys",
+            params={
+                "hostGroup": host_group,
+                "accKey": acc_key,
+                "ownerUid": owner_uid
+            })
+
+    def RevokeKey(self, host_group: str, acc_key: str, owner_uid: int = None):
+        """
+        Revokes the target key to the S3 account for the current user.
+
+        :param host_group: unique name of the target host group.
+        :param acc_key: access key that should be revoked.
+        :param owner_uid: unique identifier of the S3 account owner.
+        """
+        return self._get(
+            "RevokeKey",
+            params={
+                "hostGroup": host_group,
+                "accKey": acc_key,
+                "ownerUid": owner_uid
+            }
+        )
---
+++ tests/unit/test_api/test_s3.py
@@ -0,0 +1,100 @@
+from unittest.mock import patch, Mock
+
+import pytest
+
+from jelastic.api import S3
+
+success_response = {"error": "", "reason": 0, "result": 0, "source": "billing"}
+
+
+@pytest.fixture
+def client():
+    with patch("jelastic.api.abstract.ClientAbstract._get") as mock_get:
+        s3 = S3(session=Mock(), token="token")
+        s3._get = mock_get
+        yield s3
+
+
+def test_create(client):
+    client._get.return_value = success_response
+    response = client.Account.Create(
+        "COLUMBUS",
+        "test_project",
+        1)
+    client._get.assert_called_with(
+        "Create",
+        params={
+            "hostGroup": "COLUMBUS",
+            "name": "test_project",
+            "ownerUid": 1,
+        },
+    )
+    assert response == success_response
+
+
+def test_delete(client):
+    client._get.return_value = success_response
+    response = client.Account.Delete("COLUMBUS", "proj1", 1)
+    client._get.assert_called_with(
+        "Delete",
+        params={
+            "hostGroup": "COLUMBUS",
+            "name": "proj1",
+            "ownerUid": 1,
+        },
+    )
+    assert response == success_response
+
+
+def test_generate_keys(client):
+    client._get.return_value = success_response
+    response = client.Account.GenerateKey("COLUMBUS", "proj1", 1)
+    client._get.assert_called_with(
+        "GenerateKey",
+        params={
+            "hostGroup": "COLUMBUS",
+            "ownerUid": 1,
+            "name": "proj1",
+        },
+    )
+    assert response == success_response
+
+
+def test_get_keys(client):
+    client._get.return_value = success_response
+    response = client.Account.GetKeys(1)
+    client._get.assert_called_with(
+        "GetKeys",
+        params={
+            "ownerUid": 1,
+        },
+    )
+    assert response == success_response
+
+
+def test_regenerate_keys(client):
+    client._get.return_value = success_response
+    response = client.Account.RegenerateKeys("COLUMBUS", "access_key", 1)
+    client._get.assert_called_with(
+        "RegenerateKeys",
+        params={
+            "hostGroup": "COLUMBUS",
+            "ownerUid": 1,
+            "accKey": "access_key",
+        },
+    )
+    assert response == success_response
+
+
+def test_revoke_key(client):
+    client._get.return_value = success_response
+    response = client.Account.RevokeKey("COLUMBUS", "access_key", 123)
+    client._get.assert_called_with(
+        "RevokeKey",
+        params={
+            "hostGroup": "COLUMBUS",
+            "ownerUid": 123,
+            "accKey": "access_key",
+        },
+    )
+    assert response == success_response
