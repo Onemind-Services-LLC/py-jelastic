18ad33de8fc1a84eaaa0b96c8ac434cb71bf1203
yash-pal1 <ypal@onemindservies.com>
2023-12-19 19:54:04 +0530

    added administration.Utils

--- jelastic/api/administration/__init__.py
+++ jelastic/api/administration/__init__.py
@@ -25,6 +25,21 @@ class Administration(ClientAbstract):
             debug=self._debug,
         )
 
+    @property
+    def Utils(self) -> "_Utils":
+        """
+        >>> from jelastic import Jelastic
+        >>> jelastic = Jelastic('https://jca.xapp.cloudmydc.com', token='d6f4e314a5b5fefd164995169f28ae32d987704f')
+        >>> jelastic.administration.Utils
+
+        Ref: https://docs.jelastic.com/api/private/#!/api/administration.Utils
+        """
+        return _Utils(
+            session=self._session,
+            token=self._token,
+            debug=self._debug,
+        )
+
 
 class _Analytics(Administration):
     _endpoint2 = "analytics"
@@ -83,3 +98,277 @@ class _Analytics(Administration):
             },
             delimiter=",",
         )
+
+
+class _Utils(Administration):
+    """
+    Ref: https://docs.jelastic.com/api/private/#!/api/administration.Utils
+    """
+
+    def AddSystemExternalDNSRecord(
+        self,
+        record_data: str,
+        name: str,
+        ttl: int,
+        record_data_type: str,
+        ssl_enabled: list[bool] = None,
+        enabled: list[bool] = None,
+    ):
+        """
+        :param record_data: custom data for the DNS record.
+        :param name: name for DNS records (must be the same as one of the existing record).
+        :param ttl: TTL (time to live) value for the DNS record.
+        :param record_data_type: type of the DNS record data (currently, only the ‘TXT’ value is supported).
+        :param ssl_enabled: defines whether the SSL is enabled for the record (true, by default) or not (false).
+        :param enabled: defines whether the record is enabled (true, by default) or not (false).
+        """
+        return self._get(
+            "AddSystemExternalDNSRecord",
+            params={
+                "recordData": record_data,
+                "name": name,
+                "ttl": ttl,
+                "recordDataType": record_data_type,
+                "sslEnabled": ssl_enabled,
+                "enabled": enabled,
+            },
+            delimiter=",",
+        )
+
+    def AnalizeEnv(
+        self,
+        domain: str,
+    ):
+        return self._get(
+            "AnalizeEnv",
+            params={
+                "domain": domain,
+            },
+        )
+
+    def BalanceResources(self, domain: int):
+        return self._get("BalanceResources", params={"domain": domain})
+
+    def ClearEnvs(self):
+        return self._get("ClearEnvs", params={})
+
+    def DeleteBrokenEnvs(
+        self,
+        target_app_ids: list[str] = None,
+    ):
+        return self._get(
+            "DeleteBrokenEnvs",
+            params={
+                "targetAppIds": target_app_ids,
+            },
+            delimiter=",",
+        )
+
+    def EditSystemExternalDNSRecord(
+        self,
+        id: int,
+        record_data: list[str] = None,
+        name: list[str] = None,
+        ttl: list[int] = None,
+        record_data_type: list[str] = None,
+        ssl_enabled: list[bool] = None,
+        enabled: list[bool] = None,
+    ):
+        """
+        :param id: unique identifier of the target custom external DNS record.
+        :param record_data: new custom data for the DNS record.
+        :param name: name for DNS records (must be the same as one of the existing record).
+        :param ttl: TTL (time to live) value for the DNS record.
+        :param record_data_type: type of the DNS record data (currently, only the ‘TXT’ value is supported).
+        :param ssl_enabled: defines whether the SSL is enabled for the record (true, by default) or not (false).
+        :param enabled: defines whether the record is enabled (true, by default) or not (false).
+        """
+        return self._get(
+            "EditSystemExternalDNSRecord",
+            params={
+                "id": id,
+                "recordData": record_data,
+                "name": name,
+                "ttl": ttl,
+                "recordDataType": record_data_type,
+                "sslEnabled": ssl_enabled,
+                "enabled": enabled,
+            },
+            delimiter=",",
+        )
+
+    def ExportEnv(self, target_app_id: str):
+        return self._get(
+            "ExportEnv",
+            params={
+                "targetAppId": target_app_id,
+            },
+        )
+
+    def FixExtDns(self, uid: list[int] = None, target_app_id: list[str] = None):
+        return self._get(
+            "FixExtDns",
+            params={
+                "uid": uid,
+                "targetAppId": target_app_id,
+            },
+            delimiter=",",
+        )
+
+    def FixLaunching(self):
+        return self._get("FixLaunching", params={})
+
+    def GenerateZone(
+        self,
+        generate_slept: bool,
+    ):
+        return self._get(
+            "GenerateZone",
+            params={
+                "generateSlept": generate_slept,
+            },
+        )
+
+    def GetAvgs(self):
+        return self._get("GetAvgs", params={})
+
+    def GetAvgs2(self):
+        return self._get("GetAvgs2", params={})
+
+    def GetBalancerStat(
+        self,
+        start_time: str,
+        end_time: str,
+    ):
+        return self._get(
+            "GetBalancerStat",
+            params={
+                "starttime": start_time,
+                "endtime": end_time,
+            },
+        )
+
+    def GetCloudletsUsage(self):
+        return self._get("GetCloudletsUsage", params={})
+
+    def GetDBCreationStat(
+        self,
+        start_time: str,
+        end_time: str,
+    ):
+        return self._get(
+            "GetDBCreationStat",
+            params={
+                "startTime": start_time,
+                "endTime": end_time,
+            },
+        )
+
+    def GetErrors(
+        self,
+        start_time: str,
+        end_time: str,
+        start_row: int,
+        result_count: int,
+        filter: list[int] = None,
+    ):
+        return self._get(
+            "GerErrors",
+            params={
+                "starttime": start_time,
+                "endtime": end_time,
+                "startrow": start_row,
+                "resultCount": result_count,
+                "filter": filter,
+            },
+            delimiter=",",
+        )
+
+    def GetErrorsByDate(
+        self,
+        start_time: str,
+        end_time: str,
+        interval: int,
+        filter: list[int] = None,
+    ):
+        return self._get(
+            "GetErrorsByDate",
+            params={
+                "starttime": start_time,
+                "endtime": end_time,
+                "interval": interval,
+                "filter": filter,
+            },
+            delimiter=",",
+        )
+
+    def GetSystemExternalDNSRecords(self):
+        return self._get("GetSystemExternalDNSRecords", params={})
+
+    def GetZone(self):
+        return self._get("GetZone", params={})
+
+    def ImportEnv(
+        self,
+        env_info: str,
+        env_name: list[str] = None,
+        enable_firewall: list[bool] = None,
+    ):
+        return self._get(
+            "ImportEnv",
+            params={
+                "envInfo": env_info,
+                "envName": env_name,
+                "enableFirewall": enable_firewall,
+            },
+            delimiter=",",
+        )
+
+    def InspectEnvs(
+        self,
+        remove: list[bool] = None,
+    ):
+        return self._get(
+            "InspectEnvs",
+            params={
+                "remove": remove,
+            },
+            delimiter=",",
+        )
+
+    def ManageService(self, node_id: int, command: str, service_name: str):
+        return self._get(
+            "ManageServices",
+            params={
+                "nodeid": node_id,
+                "command": command,
+                "servicename": service_name,
+            },
+        )
+
+    def RemoveSystemExternalDNSRecord(self, id: int):
+        return self._get(
+            "RemoveSystemExternalDNSRecord",
+            params={
+                "id": id,
+            },
+        )
+
+    def UpdateEnv(self, target_app_id: str):
+        return self._get(
+            "UpdateEnv",
+            params={
+                "targetAppId": target_app_id,
+            },
+        )
+
+    def UpdateEnvInThread(self, target_app_id: str):
+        return self._get(
+            "UpdateEnvInThread",
+            params={
+                "targetAppId": target_app_id,
+            },
+        )
+
+    def UpdateNodes(self):
+        return self._get("UpdateNodes", params={})
