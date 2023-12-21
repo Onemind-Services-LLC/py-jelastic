230e1b04deb5bc791e8d26cd6823be41424abf5d
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-21 17:01:37 +0530

    lint fixes

--- jelastic/api/environment/__init__.py
+++ jelastic/api/environment/__init__.py
@@ -78,16 +78,16 @@ class Environment(ClientAbstract):
     @property
     def Binder(self) -> "_Binder":
         """
-        With the platform, you can set your own external domain name for your projects instead of using the default hosting provider domain name. Binding can be done in two ways: by adding a CNAME record or by setting A Records.
-        A CNAME specifies an alias for a canonical name record in a Domain Name System (DNS) database. If you don't have your own Public IP, your URL is an alias for a single canonical name that is associated with a common platform IP address in the DNS database. In this case, it's recommended to set your own custom domain by adding a CNAME record.
-        A Record is an entry in your DNS zone file that maps each domain name to an IP address. When you type www.mycustomsite.com, the browser goes to the nameserver for mycustomsite.com and asks for the A Record. This record must point to an IP address - it will be the IP address of your web server. Setting your own custom external domain name using A Record is more appropriate if you have a personal Public IP address.
-       Also, you can bind Custom SSL to your custom domain.
+         With the platform, you can set your own external domain name for your projects instead of using the default hosting provider domain name. Binding can be done in two ways: by adding a CNAME record or by setting A Records.
+         A CNAME specifies an alias for a canonical name record in a Domain Name System (DNS) database. If you don't have your own Public IP, your URL is an alias for a single canonical name that is associated with a common platform IP address in the DNS database. In this case, it's recommended to set your own custom domain by adding a CNAME record.
+         A Record is an entry in your DNS zone file that maps each domain name to an IP address. When you type www.mycustomsite.com, the browser goes to the nameserver for mycustomsite.com and asks for the A Record. This record must point to an IP address - it will be the IP address of your web server. Setting your own custom external domain name using A Record is more appropriate if you have a personal Public IP address.
+        Also, you can bind Custom SSL to your custom domain.
 
-        >>> from jelastic import Jelastic
-        >>> jelastic = Jelastic('https://jca.xapp.cloudmydc.com', token='d6f4e314a5b5fefd164995169f28ae32d987704f')
-        >>> jelastic.environment.Binder
+         >>> from jelastic import Jelastic
+         >>> jelastic = Jelastic('https://jca.xapp.cloudmydc.com', token='d6f4e314a5b5fefd164995169f28ae32d987704f')
+         >>> jelastic.environment.Binder
 
-        Ref: https://docs.jelastic.com/api/private/#!/api/environment.Binder
+         Ref: https://docs.jelastic.com/api/private/#!/api/environment.Binder
         """
         return _Binder(
             session=self._session,
@@ -363,17 +363,19 @@ class _Node(Environment):
         :param name: title of the message
         """
         return self._get("SendNotification", params={"name": name, "message": message})
+
+
 class _Binder(Environment):
     """
-   With the platform, you can set your own external domain name for your projects instead of using the default hosting provider domain name. Binding can be done in two ways: by adding a CNAME record or by setting A Records.
+    With the platform, you can set your own external domain name for your projects instead of using the default hosting provider domain name. Binding can be done in two ways: by adding a CNAME record or by setting A Records.
 
-    A CNAME specifies an alias for a canonical name record in a Domain Name System (DNS) database. If you don't have your own Public IP, your URL is an alias for a single canonical name that is associated with a common platform IP address in the DNS database. In this case, it's recommended to set your own custom domain by adding a CNAME record.
+     A CNAME specifies an alias for a canonical name record in a Domain Name System (DNS) database. If you don't have your own Public IP, your URL is an alias for a single canonical name that is associated with a common platform IP address in the DNS database. In this case, it's recommended to set your own custom domain by adding a CNAME record.
 
-    A Record is an entry in your DNS zone file that maps each domain name to an IP address. When you type www.mycustomsite.com, the browser goes to the nameserver for mycustomsite.com and asks for the A Record. This record must point to an IP address - it will be the IP address of your web server. Setting your own custom external domain name using A Record is more appropriate if you have a personal Public IP address.
+     A Record is an entry in your DNS zone file that maps each domain name to an IP address. When you type www.mycustomsite.com, the browser goes to the nameserver for mycustomsite.com and asks for the A Record. This record must point to an IP address - it will be the IP address of your web server. Setting your own custom external domain name using A Record is more appropriate if you have a personal Public IP address.
 
-    Also, you can bind Custom SSL to your custom domain.
+     Also, you can bind Custom SSL to your custom domain.
 
-    Ref: https://docs.jelastic.com/api/private/#!/api/environment.Binder
+     Ref: https://docs.jelastic.com/api/private/#!/api/environment.Binder
     """
 
     _endpoint2 = "Binder"
@@ -403,6 +405,7 @@ class _Binder(Environment):
             },
             delimiter=",",
         )
+
     def AddSSLCert(
         self,
         env_name: str,
@@ -441,6 +444,7 @@ class _Binder(Environment):
             },
             delimiter=",",
         )
+
     def BindExtDomain(
         self,
         env_name: str,
@@ -458,10 +462,10 @@ class _Binder(Environment):
         )
 
     def BindExtDomains(
-            self,
-            env_name: str,
-            extdomains: str,
-            cert_id: list[int] = None,
+        self,
+        env_name: str,
+        extdomains: str,
+        cert_id: list[int] = None,
     ):
         """
         param extdomain: a comma-separated list of external domains to be bound to the environment.
@@ -478,11 +482,11 @@ class _Binder(Environment):
         )
 
     def BindSSL(
-            self,
-            env_name: str,
-            cert_key: str,
-            cert: str,
-            intermediate: str,
+        self,
+        env_name: str,
+        cert_key: str,
+        cert: str,
+        intermediate: str,
     ):
         return self._get(
             "BindSSL",
@@ -491,14 +495,15 @@ class _Binder(Environment):
                 "cert_key": cert_key,
                 "cert": cert,
                 "intermediate": intermediate,
-            }
+            },
         )
+
     def BindSSLCert(
-            self,
-            env_name: str,
-            cert_id: int,
-            entry_point:list[str]=None,
-            ext_domains :list[str]=None
+        self,
+        env_name: str,
+        cert_id: int,
+        entry_point: list[str] = None,
+        ext_domains: list[str] = None,
     ):
         """
         param cert_id: unique identifier of the SSL certificate.
@@ -511,15 +516,16 @@ class _Binder(Environment):
                 "envName": env_name,
                 "certId": cert_id,
                 "entryPoint": entry_point,
-                "extDomains": ext_domains
+                "extDomains": ext_domains,
             },
             delimiter=",",
         )
+
     def CheckDomain(
-            self,
-            env_name: str,
-            domain: str,
-            region:list[str]=None,
+        self,
+        env_name: str,
+        domain: str,
+        region: list[str] = None,
     ):
         """
         param domain: domain name to be checked.
@@ -534,10 +540,11 @@ class _Binder(Environment):
             },
             delimiter=",",
         )
+
     def CheckExtDomain(
-            self,
-            env_name: str,
-            extdomains: str,
+        self,
+        env_name: str,
+        extdomains: str,
     ):
         """
         param extdomains: external domain name to be checked.
@@ -549,20 +556,21 @@ class _Binder(Environment):
                 "extdomains": extdomains,
             },
         )
+
     def DeleteSSL(
-            self,
-            env_name: str,
+        self,
+        env_name: str,
     ):
-        return self._get("DeleteSSL",
-            params={ "envName": env_name
-            },
+        return self._get(
+            "DeleteSSL",
+            params={"envName": env_name},
         )
 
     def DetachExtIp(
-            self,
-            env_name: str,
-            nodeid: int,
-            ip: str,
+        self,
+        env_name: str,
+        nodeid: int,
+        ip: str,
     ):
         """
         param nodeid: unique identifier of the target node (container).
@@ -578,20 +586,23 @@ class _Binder(Environment):
         )
 
     def DisableSSL(
-            self,
-            env_name: str,
+        self,
+        env_name: str,
     ):
-        return self._get("DisableSSL",params={
-            "envName": env_name,
-        })
+        return self._get(
+            "DisableSSL",
+            params={
+                "envName": env_name,
+            },
+        )
 
     def EditSSLCert(
-            self,
-            env_name: str,
-            id: int,
-            key: list[str]=None,
-            cert: list[str]=None,
-            interm: list[str] = None,
+        self,
+        env_name: str,
+        id: int,
+        key: list[str] = None,
+        cert: list[str] = None,
+        interm: list[str] = None,
     ):
         """
         param id: unique identifier of the target SSL certificate.
@@ -606,14 +617,15 @@ class _Binder(Environment):
                 "id": id,
                 "key": key,
                 "cert": cert,
-                "interm": interm
+                "interm": interm,
             },
             delimiter=",",
         )
+
     def GetDomainInfo(
-            self,
-            env_name: str,
-            domain: str,
+        self,
+        env_name: str,
+        domain: str,
     ):
         """
         Returns environment appid if environment found by domain.
@@ -627,12 +639,13 @@ class _Binder(Environment):
                 "domain": domain,
             },
         )
+
     def GetDomains(
-            self,
-            env_name: str,
-            node_group:list[str]=None,
-            node_id:list[int]=None,
-            in_short:list[bool]=None
+        self,
+        env_name: str,
+        node_group: list[str] = None,
+        node_id: list[int] = None,
+        in_short: list[bool] = None,
     ):
         """
         param node_group: unique identifier of the target node group (layer) for filtering, e.g. "cp" for the default application server layer.
@@ -650,37 +663,21 @@ class _Binder(Environment):
             delimiter=",",
         )
 
-    def GetExtDomains(
-            self,
-            env_name: str
-    ):
-        return self._get("GetExtDomains", params={
-            "envName": env_name
-        })
+    def GetExtDomains(self, env_name: str):
+        return self._get("GetExtDomains", params={"envName": env_name})
 
-    def GetSSL(
-            self,
-            env_name: str
-    ):
-        return self._get("GetSSL", params={
-            "envName": env_name
-        })
-    def GetSSLCerts(
-            self,
-            env_name: str,
-            ids:list[str]=None
-    ):
-        return self._get("GetSSLCerts", params={
-            "envName": env_name,
-            "ids":ids},
-                         delimiter=",",
-                         )
+    def GetSSL(self, env_name: str):
+        return self._get("GetSSL", params={"envName": env_name})
+
+    def GetSSLCerts(self, env_name: str, ids: list[str] = None):
+        return self._get(
+            "GetSSLCerts",
+            params={"envName": env_name, "ids": ids},
+            delimiter=",",
+        )
 
     def ManageNodeDnsState(
-            self,
-            env_name:str,
-            node_id: list[int] = None,
-            enabled: list[bool] = None
+        self, env_name: str, node_id: list[int] = None, enabled: list[bool] = None
     ):
         """
         param env_name: target environment name.
@@ -696,12 +693,9 @@ class _Binder(Environment):
             },
             delimiter=",",
         )
+
     def MoveExtIps(
-            self,
-            env_name:str,
-            source_node_id:int,
-            target_node_id: int,
-            ips: str
+        self, env_name: str, source_node_id: int, target_node_id: int, ips: str
     ):
         """
         param env_name: source environment name.
@@ -715,15 +709,16 @@ class _Binder(Environment):
                 "envName": env_name,
                 "sourceNodeId": source_node_id,
                 "targetNodeId": target_node_id,
-                "ips":ips
+                "ips": ips,
             },
         )
+
     def RemoveDomains(
-            self,
-            env_name:str,
-            domains:str,
-            node_group: list[str] = None,
-            node_id: list[int] = None,
+        self,
+        env_name: str,
+        domains: str,
+        node_group: list[str] = None,
+        node_id: list[int] = None,
     ):
         """
         param env_name: target environment name.
@@ -741,10 +736,11 @@ class _Binder(Environment):
             },
             delimiter=",",
         )
+
     def RemoveExtDomains(
-            self,
-            env_name:str,
-            extdomain:str,
+        self,
+        env_name: str,
+        extdomain: str,
     ):
         """
         param env_name: target environment name.
@@ -757,9 +753,10 @@ class _Binder(Environment):
                 "extdomain": extdomain,
             },
         )
+
     def RemoveSSL(
-            self,
-            env_name:str,
+        self,
+        env_name: str,
     ):
         """
         param env_name: target environment name.
@@ -770,10 +767,11 @@ class _Binder(Environment):
                 "envName": env_name,
             },
         )
+
     def RemoveSSLCerts(
-            self,
-            env_name:str,
-            ids:str,
+        self,
+        env_name: str,
+        ids: str,
     ):
         """
         param env_name: target environment name.
@@ -786,13 +784,14 @@ class _Binder(Environment):
                 "ids": ids,
             },
         )
+
     def SetExtIpCount(
-            self,
-            env_name:str,
-            type:str,
-            count: int,
-            node_group: list[str] = None,
-            node_id: list[int] = None,
+        self,
+        env_name: str,
+        type: str,
+        count: int,
+        node_group: list[str] = None,
+        node_id: list[int] = None,
     ):
         """
         param env_name: target environment name.
@@ -806,16 +805,17 @@ class _Binder(Environment):
             params={
                 "envName": env_name,
                 "type": type,
-                "count":count,
+                "count": count,
                 "nodeGroup": node_group,
                 "node_id": node_id,
             },
             delimiter=",",
         )
+
     def SwapExtDomains(
-            self,
-            env_name:str,
-            targetappid:str,
+        self,
+        env_name: str,
+        targetappid: str,
     ):
         """
         param env_name: target environment name.
@@ -830,12 +830,12 @@ class _Binder(Environment):
         )
 
     def SwapExtIps(
-            self,
-            env_name:str,
-            source_node_id:int,
-            target_node_id: int,
-            source_ip: list[str]=None,
-            target_ip: list[str]=None
+        self,
+        env_name: str,
+        source_node_id: int,
+        target_node_id: int,
+        source_ip: list[str] = None,
+        target_ip: list[str] = None,
     ):
         """
         param env_name: source environment name.
@@ -850,15 +850,16 @@ class _Binder(Environment):
                 "envName": env_name,
                 "sourceNodeId": source_node_id,
                 "targetNodeId": target_node_id,
-                "sourceIp":source_ip,
-                "targetIp":target_ip
+                "sourceIp": source_ip,
+                "targetIp": target_ip,
             },
             delimiter=",",
         )
+
     def UnbindSSLCert(
-            self,
-            env_name:str,
-            extdomains:list[str]=None,
+        self,
+        env_name: str,
+        extdomains: list[str] = None,
     ):
         """
         param env_name: target environment name.
@@ -1056,4 +1057,3 @@ class _Security(Environment):
             },
             delimiter=",",
         )
-
