b991366c199351d702a88744aaf4e468d7d7fb81
Abhimanyu Saharan <asaharan@onemindservices.com>
2024-01-12 01:19:12 +0530

    Update setup.py
    Signed-off-by: Abhimanyu Saharan <asaharan@onemindservices.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="py-jelastic",
-    version="0.1.2-dev",
+    version="0.1.2",
     description="Jelastic API client library",
     long_description=long_description,
     long_description_content_type="text/markdown",
